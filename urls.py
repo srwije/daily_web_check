74c3ae05f40aa20e73c56507246bcbac3852ca4f
Ravinda Wijethilake <37626925+srwije@users.noreply.github.com>
2025-04-24 09:13:30 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,18 +5,18 @@ urls = [
     'https://backend.hcmsmiles.lk/login',
     'https://hcmsmiles.lk/login',
     'https://backend.kandos.lk/backend/login',
-    'https://kandos.lk/',
+    'https://kandos.lk',
     'http://124.43.177.3:3000/',
     'https://tyreshoponline.lk/backend/login',
-    'https://tyreshoponline.lk/',
-    'https://fleetmgt.slt.lk/',
-    'https://routeradar.trackfleet.online/',
-    'https://www.ophtha.health/',
-    'https://online.trinitycollege.lk/',
-    'https://www.winaluxurytriptosrilanka.com.au/',
+    'https://tyreshoponline.lk',
+    'https://fleetmgt.slt.lk',
+    'https://routeradar.trackfleet.online',
+    'https://www.ophtha.health',
+    'https://online.trinitycollege.lk',
+    'https://www.winaluxurytriptosrilanka.com.au',
     'https://koding.lk',
     'https://keerthikodithuwakku.com',
     'https://effectivesolutions.lk',
-    'https://www.jendoinnovations.com/'
-
+    'https://www.jendoinnovations.com',
+    'https://www.jendo.health'
 ]
